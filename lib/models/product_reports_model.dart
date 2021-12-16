959f91ab1deec92437873a1f616bf9612d0a99e8
Сорокин Владимир <sorokin@it-apriori.ru>
2021-12-16 17:12:28 +0300

    add class productArea

---
+++ lib/models/product_reports_model.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ProductRepostsModel extends ChangeNotifier {}
+
+class ProductRepost {
+  DateTime data;
+  String number;
+  String productArea;
+  String comment;
+  List<Nomenclature> nomenclature = [];
+
+  ProductRepost(
+      {required this.data,
+      required this.number,
+      required this.productArea,
+      required this.comment});
+}
+
+class Nomenclature {
+  String barcode;
+  String name;
+  String count;
+
+  Nomenclature(
+      {required this.barcode, required this.name, required this.count});
+}
