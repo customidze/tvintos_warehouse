699d61272d391b148f7b11907eb43f9fd3b6d4a1
Сорокин Владимир <sorokin@it-apriori.ru>
2021-12-23 17:01:50 +0300

    repair datapicker

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:tvintos_warehouse/models/product_reports_model.dart';
@@ -6,7 +7,7 @@ import 'package:tvintos_warehouse/models/settings_model.dart';
 import 'package:tvintos_warehouse/pages/main_page.dart';
 import 'package:tvintos_warehouse/pages/settings_page.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   runApp(const MyApp());
@@ -19,6 +20,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       child: MaterialApp(
+        localizationsDelegates: [GlobalMaterialLocalizations.delegate],
+        supportedLocales: [const Locale('en'), const Locale('ru')],
         title: 'Tvintos WareHouse',
         theme: ThemeData(primarySwatch: Colors.green),
         home: MainPage(),
